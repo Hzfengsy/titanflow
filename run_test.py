59f10a0aeae338958aa00228b34370398b0850ef
Hzfengsy <Hzfengsy@vip.qq.com>
2017-08-08 19:41:52 +0800

    for linux

--- run_test.py
+++ run_test.py
@@ -4,33 +4,33 @@ import sys
 python_cmd = "python"
 testcase_dir = "testcase"
 tests = [
-		 ["adder",       "1_adder.py"],
+		 ["adder",	   "1_adder.py"],
 		 ["initializer", "2_init.py"],
-		 ["assign",      "3_assign.py"],
-		 ["context",     "4_context.py"],
-		 ["autodiff",    "5_mnist_grad.py"],
+		 ["assign",	  "3_assign.py"],
+		 ["context",	 "4_context.py"],
+		 ["autodiff",	"5_mnist_grad.py"],
 		 ["optimizer",   "6_mnist_optimizer.py"],
 		 ["multilayer perceptron", "7_ml_perceptron.py"],
-		 ["adam optimizer",        "8_adam.py"],
-		 ["CNN 0",       "9_cnn_0.py"],
-		 ["CNN 1",       "10_cnn_1.py"],
-		 ["CNN 2",       "11_cnn_2.py"]
+		 ["adam optimizer",		"8_adam.py"],
+		 ["CNN 0",	   "9_cnn_0.py"],
+		 ["CNN 1",	   "10_cnn_1.py"],
+		 ["CNN 2",	   "11_cnn_2.py"]
 		 ]
 
 def main(model_name):
 	for i, item in enumerate(tests):
-	test_name = item[0]
-	file_name = item[1]
-	
-	print("========== test %d %s ==========" % (i+1, test_name))
-	
-	os.system("cp %s %s" % (os.path.join(testcase_dir, file_name),
-							file_name))
-							os.system("sed -i 's/your_model/%s/' %s" % (model_name, file_name))
-							ret = os.system("%s %s" % (python_cmd, file_name))
-							if ret != 0:
-								exit(0)
-							os.system("rm %s" % (file_name))
+		test_name = item[0]
+		file_name = item[1]
+		
+		print("========== test %d %s ==========" % (i+1, test_name))
+		
+		os.system("cp %s %s" % (os.path.join(testcase_dir, file_name),
+								file_name))
+								os.system("sed -i 's/your_model/%s/' %s" % (model_name, file_name))
+								ret = os.system("%s %s" % (python_cmd, file_name))
+								if ret != 0:
+									exit(0)
+								os.system("rm %s" % (file_name))
 print("")
 	print("Pass all!")
 
